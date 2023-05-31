66da268ddd32859fb8259b09857c532e67226e86
Salvador Gonzalez <gsalvadorcr0322@gmail.com>
2023-05-31 13:43:25 -0500

    Le dimos funcionalidad a los botones de anterior y siguiente

--- js/index.js
+++ js/index.js
@@ -18,9 +18,7 @@ const getPokemon = async (urlApi) =>{
         console.error(e);
     }//Utilizamos el try catch para la prueba de errores y ver si la estamos cagando
 }
-
 getPokemon(`${API}/?limit=12&offset=0`)
-
  //Comenzaremos a hacer la funcion para obtener todos los datos del pokemon
  const dataPoke = async(data) => {
     try{
@@ -28,13 +26,13 @@ getPokemon(`${API}/?limit=12&offset=0`)
             const response = await fetch(index.url); // realizamos fetch a cada dato que obtenemos como url
             const result = await response.json();
             console.log(result);
-            let {name,id} = result;
+            let {name,id} = result; //Hacemos destructuring a nuestro array para ingresar mas facil a las variables
             let vista = `
                 <div class="card col-4 me-2 mb-2" style="width: 18rem;">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="...">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="Imagen de Pokemon ${name}">
                     <div class="card-body">
                         <h5 class="card-title">${name}</h5>
-                        <p class="card-text fs-4"><img class="img-fluid" src="./src/img/icono-pokedex.png" alt="">${id}</p>
+                        <p class="card-text fs-4"><img class="img-fluid" src="./src/img/icono-pokedex.png" alt="Imagen de Pokebola">${id}</p>
                         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">Srobre el</button>
                     </div>
                 </div>
@@ -45,3 +43,12 @@ getPokemon(`${API}/?limit=12&offset=0`)
         console.error(error);
     }
  }
+
+ //Añadiendo la funcionalidad a los botones anterior y siguiente
+ pagination.addEventListener('click', (e) =>{//Añadimos un evenListener a nuestra pagination para que escuche cuando demos click
+    if(e.target.matches('.page-link')){ //Mientras que nuestro objeto contenga una clase llamada page-link sigue
+        let value = e.target.dataset.url; //Asignamos el url a value
+        contenedor.innerHTML=''; //Vaciamos nuestro contenedor para que quede vacio
+        getPokemon(value); //Volvemos a llamar a getPokemon para que nos muestre los siguientes pokemon
+    }
+ })
