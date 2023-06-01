e2a34d170b0f79d61d25a0b671456b5e469556af
Salvador Gonzalez <gsalvadorcr0322@gmail.com>
2023-06-01 03:02:41 -0500

    Se agrega un bloque de codigo que envia los datos de los pokemons cargados a LS para que sea mas facil hacer el modal sin muchas peticiones fetch

--- js/index.js
+++ js/index.js
@@ -15,22 +15,26 @@ const getPokemon = async (urlApi) =>{
         btnNext = results.next ?`<li class="page-item"><a class="page-link" data-url=${results.next}>Siguiente</a></li>` : '';
         btnPrevious=results.previous ? `<li class="page-item"><a class="page-link" data-url=${results.previous}>Anterior</a></li>` : ''
         pagination.innerHTML=btnPrevious + " " + btnNext;
+        pagination.style.cursor = 'pointer';
     }catch(e){
         console.error(e);
     }//Utilizamos el try catch para la prueba de errores y ver si la estamos cagando
 }
 getPokemon(`${API}/?limit=12&offset=0`)
  //Comenzaremos a hacer la funcion para obtener todos los datos del pokemon
+ var obtData=[];
  const dataPoke = async(data) => {
     Spinner.style.visibility = 'visible';//Se muestra el spinner y se oculta el contenedor y la paginacion
     contenedor.style.visibility = 'hidden';
     pagination.style.visibility = 'hidden';
+    let Objeto;
     try{
         for(let index of data){//Iteramos sobre la data que nos trae el documento
             const response = await fetch(index.url); // realizamos fetch a cada dato que obtenemos como url
             const result = await response.json();
             console.log(result);
-            let {name,id} = result; //Hacemos destructuring a nuestro array para ingresar mas facil a las variables
+            let {name,id,stats} = result; //Hacemos destructuring a nuestro array para ingresar mas facil a las variables
+            objeto={id,name,stats}
             let vista = `
                 <div class="card col-4 me-2 mb-2" style="width: 18rem;">
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="Imagen de Pokemon ${name}">
@@ -42,8 +46,11 @@ getPokemon(`${API}/?limit=12&offset=0`)
                 </div>
             `;
             contenedor.innerHTML += vista;//Comenzamos a agregar la vista a nuestro contenedor, siempre sumando para que muestre todo
+            obtData.push(objeto);
+
         }
         await Loader();//Una vez cargadas las 12 cards llama a Loader para que muestre todo
+        await setLocalStorage();
     }catch(error){
         console.error(error);
     }
@@ -54,6 +61,7 @@ getPokemon(`${API}/?limit=12&offset=0`)
     if(e.target.matches('.page-link')){ //Mientras que nuestro objeto contenga una clase llamada page-link sigue
         let value = e.target.dataset.url; //Asignamos el url a value
         contenedor.innerHTML=''; //Vaciamos nuestro contenedor para que quede vacio
+        obtData=[];
         getPokemon(value); //Volvemos a llamar a getPokemon para que nos muestre los siguientes pokemon
     }
  })
@@ -67,4 +75,10 @@ const Loader = async () =>{
         },1500);
         
     })
+}
+const setLocalStorage = async () =>{
+    for(let i = 0; i<obtData.length;i++){
+        console.log(obtData[i]);
+        localStorage.setItem(`dataPoke${i}`,JSON.stringify(obtData[i]));
+    }
 }
